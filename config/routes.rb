6e948858295d15aa1acf8ee534e21f515f04663e
orikotanaka <oriko644@gmail.com>
2023-06-02 14:20:49 +0900

    devise実装その１

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root "homes#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  
+  devise_for :users, controllers: {
+    # UserのSessionsControllerには、Users::SessionsControllerを利用する。他のコントローラーも同じように修正する。
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
 end
